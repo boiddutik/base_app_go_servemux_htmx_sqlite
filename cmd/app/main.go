af062ca787e676f96a6f81068cc9c60e83a31540
Nuaiman Ashiq <ashiqnuaiman@gmail.com>
2025-07-28 02:11:14 +0600

    commit 4

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -9,20 +9,15 @@ import (
 	"github.com/boiddutik/go_htmx/crud_app/internal/routes"
 )
 
-var tmpl *template.Template
-
-func init() {
-	db.OpenDB()
-	tmpl = template.Must(template.ParseGlob("ui/templates/*.html"))
-	log.Println("✅ DB initialized and templates parsed")
-}
-
 func main() {
+	db.OpenDB()
 	defer db.DB.Close()
+	log.Println("✅ Connected to SQLite database.")
+
+	tmpl := template.Must(template.ParseGlob("ui/templates/*.html"))
 
 	mux := http.NewServeMux()
 	mux.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("ui/static"))))
-
 	routes.SetupRoutes(mux, tmpl)
 
 	log.Println("🚀 Server running at http://localhost:8000")
